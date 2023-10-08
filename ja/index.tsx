617d56c990e8f2d6e7b355466fa79fd2e7fff967
kurata321 <kurata.hironori321@gmail.com>
2023-10-08 23:37:54 +0900

    add mdx files for border-radius linear-gradient

--- ja/index.tsx
+++ ja/index.tsx
@@ -1,7 +1,11 @@
 import Index from './index/index.mdx'
 import Border from './border/index.mdx'
+import BorderRadius from './border-radius/index.mdx'
+import LinearGradient from './linear-gradient/index.mdx'
 
 export {
   Index,
-  Border
+  Border,
+  BorderRadius,
+  LinearGradient,
 }
